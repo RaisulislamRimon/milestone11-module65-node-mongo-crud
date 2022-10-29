4b641c211180364bdab799e1e4a883d94233dc11
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-29 16:58:52 +0000

    module65-1

--- module65-1/node-mongo-crud-server/index.js
+++ module65-1/node-mongo-crud-server/index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+import { MongoClient } from "mongodb";
+ 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+// async await
+async function run() {
+  fetch(``)
+  try {
+
+  }
+  catch() {
+
+  }
+  finally{
+    
+  }
+}
+
+run().catch((error) => console.error(error));
 
 app.get("/", (req, res) => {
   res.send(`Hello from node mongodb crud server ${port}`);
