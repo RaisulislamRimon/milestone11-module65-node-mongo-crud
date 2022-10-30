5071182a943b128ad62ce26f897b05edd9bb997a
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-30 10:23:35 +0000

    create an add user form

--- module65-3/node-mongo-crud-client/src/components/AddUser.js
+++ module65-3/node-mongo-crud-client/src/components/AddUser.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 const AddUser = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
       <h2>Please add a new user </h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" id="name" placeholder="name" />
+        <br />
+        <input type="email" name="email" id="email" placeholder="email" />
+        <br />
+        <input type="submit" value="Add user" />
+      </form>
     </div>
   );
 };
