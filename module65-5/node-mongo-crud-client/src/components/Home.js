0ec9b67bcbfaa1d870cd20ece210414365422d19
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-30 11:41:36 +0000

    want to delete from mongodb server.

--- module65-5/node-mongo-crud-client/src/components/Home.js
+++ module65-5/node-mongo-crud-client/src/components/Home.js
@@ -3,13 +3,18 @@ import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
   const users = useLoaderData();
+
+  const handleDelete = (_id) => {
+    console.log(`deleting user with _id`, _id);
+  };
   return (
     <div>
       <h2>Users : {users.length} </h2>
       <div>
         {users.map((user) => (
           <p key={user._id}>
-            {user.name} {user.email}
+            {user.name} {user.email}{" "}
+            <button onClick={() => handleDelete(user._id)}>X</button>
           </p>
         ))}
       </div>
