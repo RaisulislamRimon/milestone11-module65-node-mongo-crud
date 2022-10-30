04211a83882220d8fd479e13b4b47734cc68cd08
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-30 11:36:02 +0000

    getting user from server to client

--- module65-5/node-mongo-crud-client/src/components/Home.js
+++ module65-5/node-mongo-crud-client/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
+  const users = useLoaderData();
   return (
     <div>
-      <h2>Users : </h2>
+      <h2>Users : {users.length} </h2>
     </div>
   );
 };
