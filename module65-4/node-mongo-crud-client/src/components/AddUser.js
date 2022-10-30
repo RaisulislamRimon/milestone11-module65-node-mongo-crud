56757cb3d113d6266e05352a962f2bdae07775ad
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-30 10:43:23 +0000

    project init

---
+++ module65-4/node-mongo-crud-client/src/components/AddUser.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const AddUser = () => {
+  // const [user, setUser] = useState({ name: "R", email: "r@gmail.com" });
+  const [user, setUser] = useState({});
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  const handleInputChange = (e) => {
+    // console.log(e.target);
+    const value = e.target.value;
+    const field = e.target.name;
+    // console.log(value, field);
+    const newUser = { ...user };
+    newUser[field] = value;
+    setUser(newUser);
+    // console.log(newUser);
+  };
+
+  return (
+    <div>
+      <h2>Please add a new user </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleInputChange}
+          type="text"
+          name="name"
+          id="name"
+          placeholder="name"
+        />
+        <br />
+        <input
+          onChange={handleInputChange}
+          type="text"
+          name="address"
+          id="address"
+          placeholder="address"
+        />
+        <br />
+        <input
+          onChange={handleInputChange}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="email"
+        />
+        <br />
+        <input type="submit" value="Add user" />
+      </form>
+    </div>
+  );
+};
+
+export default AddUser;
